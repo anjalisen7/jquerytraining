47b1fbd92423ad674ef74acea91c00e954cc0295
anjali07 <asen53184@gmail.com>
2021-05-20 17:25:09 +0530

    jquery

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+   
+  
+  
+   function italic(){
+    var todo = document.getElementById("txt").value;
+   var x= document.createElement('li');
+    x.textContent = todo;
+    var add =document.getElementById("demo");
+     add.before(x);
+  
+   }
+
+
+   
+   
+
+   
+ 
